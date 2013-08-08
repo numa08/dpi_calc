c207d4716698d2cc00cafed5ed2b0c2472549b5a
funabiki.t <funabiki.t@isoroot.jp>
2013-08-08 06:19:11 +0100

    Dpiの定義クラスを修正

--- application/DpiDroid/app/models/Dpi.scala
+++ application/DpiDroid/app/models/Dpi.scala
@@ -3,14 +3,14 @@ package models
 case class Dpi(value : Double){
   
   def pow = {
-    math.pow(value, 2)
+    Dpi(math.pow(value, 2))
   }
 
   def +(target : Dpi) = {
-    value + target.value
+    Dpi(value + target.value)
   }
 
   def sqrt = {
-    math.sqrt(value)
+    Dpi(math.sqrt(value))
   }
 }
