cfb6e215a9ed38c4164b32f193b9a61a63638807
funabiki.t <funabiki.t@isoroot.jp>
2013-08-08 07:24:48 +0100

    テンプレートエンジン

--- application/DpiDroid/app/controllers/Application.scala
+++ application/DpiDroid/app/controllers/Application.scala
@@ -2,6 +2,7 @@ package controllers
 
 import models._
 import play.api._
+import play.api.templates._
 import play.api.mvc._
 
 object Application extends Controller {
@@ -11,7 +12,8 @@ object Application extends Controller {
     val height = 640
     val inch = 4
     val dpi = new Display(width, height, inch).dpi
-    Ok(dpi.toString)
+    val content = new StringBuilder().append("<p>").append(dpi).append("</p>")
+    Ok(views.html.main.render("hello dpi", new Html(content)))
   }
   
 }
