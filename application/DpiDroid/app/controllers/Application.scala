af94f7d0cfd9ebe59d1841e065be54aad85e0af0
funabiki.t <funabiki.t@isoroot.jp>
2013-08-09 02:29:49 +0100

    Getで計算をする

--- application/DpiDroid/app/controllers/Application.scala
+++ application/DpiDroid/app/controllers/Application.scala
@@ -13,7 +13,12 @@ object Application extends Controller {
     val inch = 4
     val dpi = new Display(width, height, inch).dpi
     val content = new StringBuilder().append("<p>").append(dpi).append("</p>")
-    Ok(views.html.main.render("hello dpi", new Html(content)))
+    Ok(views.html.main.render)
+  }
+
+  def calc(width: Long, height: Long, inch: Long) = Action {
+    val dpi = new Display(width, height, inch).dpi
+    Ok(dpi.toString)
   }
   
 }
